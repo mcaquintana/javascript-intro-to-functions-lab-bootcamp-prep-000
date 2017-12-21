65e7ff9f221e4073560c2d9670e05766d920a9a2
Monica Quintana-Stiles <mca_quintana@yahoo.com>
2017-12-21 18:37:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function sayHiToGrandma(string){
     return "YES INDEED!"
   }
   
-  if (string === "I love you, Grandma"){
+  else (string === "I love you, Grandma"){
     return "I love you, too."
   }
 }
